1dba2c205ddc29dee4bb1c99183ffd6ff10fa98b
Ismael ABBO <ismaelabbo46@gmail.com>
2022-11-28 22:09:00 +0100

    Refactoring and positioning elements

--- js/app.js
+++ js/app.js
@@ -3,17 +3,22 @@ console.log(`I ran`);
 const
 	rotationAngle	= .75,
 	edgeReduction	= .49,
-	widthReduction	= .45
+	widthReduction	= .45,
+
+	arcadeContainer	= document.querySelector(`#arcade-container`)
 ;
 
 function setup() 
 {
-	createCanvas(500, 500);
+	createCanvas(500, 500)
+		.parent(`#arcade-container`)
+	;
 	background(0, 0, 30);
-	drawEdge(width / 2, height - 50, 0, 150, 75);
+	drawRootTrunk(width / 2, height - 50, 0, 75, 70);
+	drawEdge(width / 2, height - 150, 0, 75, 150);
 }
 
-function drawEdge(x, y, angle, height, width)
+function drawEdge(x, y, angle, width, height)
 {
 	if(height < 2) return;
 
@@ -26,6 +31,18 @@ function drawEdge(x, y, angle, height, width)
 	vector.rotate(angle);
 	line(x, y, x + vector.x, y + vector.y);
 
-	drawEdge(x + vector.x, y + vector.y, angle + rotationAngle, height * edgeReduction, width * widthReduction);
-	drawEdge(x + vector.x, y + vector.y, angle - rotationAngle, height * edgeReduction, width * widthReduction);
+	drawEdge(x + vector.x, y + vector.y, angle + rotationAngle, width * widthReduction, height * edgeReduction);
+	drawEdge(x + vector.x, y + vector.y, angle - rotationAngle, width * widthReduction, height * edgeReduction);
+}
+
+function drawRootTrunk(x, y, angle, width, height)
+{
+	noFill();
+	stroke(255);
+	strokeWeight(width);
+
+	let vector	= createVector(0, -height);
+
+	vector.rotate(angle);
+	line(x, y, x + vector.x, y + vector.y);
 }
